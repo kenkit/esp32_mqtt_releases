8fb84a574e01101d9d3ce5d43b248bc6f22aba48
๖̶̶̶ۣۣۜۜ͜ζ͜͡ Sage <kkituyi@hotmail.com>
2025-11-27 18:58:35 +0300

    fix: Use correct pylzss functions for compression/decompression

--- lzss.py
+++ lzss.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python3
 
-import platform
 import sys
-import ctypes
 import lzss
 
 if len(sys.argv) != 4:
     print ("Usage: lzss.py --[encode|decode] infile outfile")
     sys.exit()
+
 mode   = sys.argv[1]
 ifile  = sys.argv[2]
 ofile  = sys.argv[3]
 
-b_ifile = ifile.encode('utf-8')
-b_ofile = ofile.encode('utf-8')
+with open(ifile, 'rb') as f_in:
+    data = f_in.read()
 
 if mode == "--encode":
-     lzss.encode_file.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-     lzss.encode_file(b_ifile, b_ofile)
+    processed_data = lzss.compress(data)
 elif mode == "--decode":
-     lzss.decode_file.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
-     lzss.decode_file(b_ifile, b_ofile)
+    processed_data = lzss.decompress(data)
 else:
     print ("Error, invalid mode parameter, use --encode or --decode")
+    sys.exit(1)
+
+with open(ofile, 'wb') as f_out:
+    f_out.write(processed_data)
+
